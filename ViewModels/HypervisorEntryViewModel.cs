23e04dc07a6b27a51c7edb067672436f32da4ce2
atshaw1994 <atshaw1994@gmail.com>
2026-07-25 04:14:09 -0400

    Add HypervisorEntry ViewModel and UserControl
    Introduced HypervisorEntryViewModel with editable name support using CommunityToolkit.Mvvm attributes and commands (Edit, Save, Cancel, Browse). Added HypervisorEntry.xaml UserControl for UI binding and editing, along with a minimal code-behind file for initialization.

---
+++ ViewModels/HypervisorEntryViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace VMUpdater.ViewModels
+{
+    public partial class HypervisorEntryViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public partial string HypervisorName { get; set; } = "Hypervisor";
+
+        [ObservableProperty]
+        public partial string TempName { get; set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial bool IsEditingName { get; set; }
+
+        [RelayCommand]
+        private void EditName()
+        {
+            TempName = HypervisorName;
+            IsEditingName = true;
+        }
+
+        [RelayCommand]
+        private void SaveName()
+        {
+            HypervisorName = TempName;
+            IsEditingName = false;
+        }
+
+        [RelayCommand]
+        private void CancelEdit()
+        {
+            IsEditingName = false;
+        }
+
+        [RelayCommand]
+        public void Browse()
+        {
+
+        }
+    }
+}
